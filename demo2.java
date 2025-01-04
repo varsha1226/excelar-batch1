b8e6a06d70b862e9e736f5236f77ab30c40770bb
varsha1226 <boyapativarsha26@gmail.com>
2025-01-04 12:35:38 +0530

    Add files via upload

---
+++ demo2.java
@@ -0,0 +1,13 @@
+class demo2{
+    public static void main(string[]args){
+    int a = 10;
+    intb = 5;
+    if(a>b){
+        System.out.println("A is greater than B");
+    }
+        else{
+        System.out.println("B is greater than A");
+    
+    }
+} 
+}   
---
+++ demo5.java
@@ -0,0 +1,14 @@
+package core_java;
+
+public class Demo5 {
+    public static void main(String[] args) {
+          String Username = "Ak";
+          String pwd = "varsha";
+          while((Username == "Ak") && (pwd =="varsha")) {
+            System.out.println("hello varshu Happy new year");
+            break;
+          }
+
+    }
+    
+}
