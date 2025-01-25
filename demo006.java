6ef905c1375a6382f2d7d06f79b9000397fcc17b
varsha1226 <boyapativarsha26@gmail.com>
2025-01-25 10:56:58 +0530

    Add files via upload

---
+++ demo006.java
@@ -0,0 +1,10 @@
+public class demo006 {
+    public static void main(String[] args) {
+        String Username = "varsha";
+        String pwd = "varshu";
+        while((Username == "varsha") && (pwd =="varshu")) {
+          System.out.println("Welcome to malla reddy university..");
+          break;
+        }
+
+  }
