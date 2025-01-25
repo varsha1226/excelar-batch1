993d2daccd37fee4dc6f73f9e4b7855780c30f81
varsha1226 <boyapativarsha26@gmail.com>
2025-01-25 10:55:46 +0530

    Add files via upload

---
+++ demo002.java
@@ -0,0 +1,12 @@
+class demo02 {
+    public static void main(String[] args) {       
+        int numx = 20; 
+        int y = 18;
+        
+        if (numx > y) {  
+            System.out.println("x is greater than y");
+        } else {  
+            System.out.println("y is greater than x");
+        }
+    }
+}
