8261e0c677d80a10c76210434b420e809c716bfd
varsha1226 <boyapativarsha26@gmail.com>
2025-01-25 10:56:40 +0530

    Add files via upload

---
+++ demo005.java
@@ -0,0 +1,10 @@
+public class demo005 {
+    public static void main(String[] args) {
+          String Username = "varshuu";
+          String pwd = "varsha";
+          while((Username == "varshuu") && (pwd =="varsha")) {
+            System.out.println("hello varshuu happy new year");
+            break;
+          }
+
+    }
