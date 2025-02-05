0290477eec978b31979a0602a7ccb66137e66a5f
varsha1226 <boyapativarsha26@gmail.com>
2025-02-05 11:25:44 +0530

    Add files via upload

---
+++ demo028.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class demo028 {
+    
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of terms in the Fibonacci series: ");
+        int n = sc.nextInt();
+        System.out.println("Fibonacci Series up to " + n + " terms:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+}
